42a707e1f3743cdba16592ffe93589611c911745
Qasim AJ <qasimmohammad534@gmail.com>
2026-04-28 20:12:12 +0500

    Remove testing comments from turtle.js

--- turtle/turtle.js
+++ turtle/turtle.js
@@ -6,7 +6,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//testing commits
 
 
 class turtle {
